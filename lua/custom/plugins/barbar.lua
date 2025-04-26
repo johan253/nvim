53ce544b128066301f5292fe6f51d32a110f1045
johan253 <johannjo2000@gmail.com>
2025-04-26 16:27:22 -0700

    Added buffer tab `barbar`

---
+++ lua/custom/plugins/barbar.lua
@@ -0,0 +1,23 @@
+local map = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
+
+map('n', '<leader>w', '<Cmd>w<CR>', { desc = '[W]rite Buffer', noremap = true, silent = true })
+map('n', '<leader>q', '<Cmd>BufferClose<CR>', { desc = '[Q]uit Buffer', noremap = true, silent = true })
+
+return {
+  'romgrk/barbar.nvim',
+  dependencies = {
+    'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
+    'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
+  },
+  init = function()
+    vim.g.barbar_auto_setup = true
+  end,
+  opts = {
+    -- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
+    -- animation = true,
+    -- insert_at_start = true,
+    -- …etc.
+  },
+  version = '^1.0.0', -- optional: only update when a new 1.x version is released
+}
