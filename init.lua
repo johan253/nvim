1baf9a514280019579091b33e5c43b2594abe0cd
johan253 <johannjo2000@gmail.com>
2025-04-26 15:16:15 -0700

    Changed colorscheme and indent line character

--- init.lua
+++ init.lua
@@ -956,7 +956,7 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'github_dark_dimmed'
+      vim.cmd.colorscheme 'github_dark_default'
     end,
   },
 
